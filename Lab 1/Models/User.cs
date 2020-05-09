54f49c84b7f935cb9ea9395266aedf72d81cd306
mihairuncan <runcan.mihai@gmail.com>
2020-05-09 20:24:23 +0300

    Add project files.

---
+++ Lab 1/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_1.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        [EmailAddress]
+        [MinLength(10)]
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string password { get; set; }
+        public DateTime LastLoginTime { get; set; }
+    }
+}
